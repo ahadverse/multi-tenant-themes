b921347cc30cf250fec8ec8ddd8a5d4bfa39b8fb
Jahid Hiron <jahidul@ewwfl.com>
2026-01-07 01:43:03 +0600

    Theme issue

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,38 +1,39 @@
 import Link from "next/link";
+import { readFile } from "fs/promises";
+import path from "path";
 
-export default function RootPage() {
+async function getTenants() {
+  const filePath = path.join(process.cwd(), "public", "data.json");
+  const file = await readFile(filePath, "utf-8");
+  return JSON.parse(file);
+}
+
+export default async function RootPage() {
+  const data = await getTenants();
+  const tenants = data.tenants;
+
+  const baseDomain = process.env.NEXT_PUBLIC_LIVEURL || "localhost:3000";
+
+  const protocol = baseDomain.includes("localhost") ? "http" : "https";
+
   return (
     <div className='flex items-center justify-center min-h-screen font-sans'>
       <div className='text-center border p-10 rounded-xl shadow-sm'>
         <h1 className='text-2xl font-bold mb-2'>Multi-tenant Engine</h1>
         <p className='text-gray-600'>Please visit a tenant site:</p>
-        <div className='mt-4 space-y-2 space-x-2'>
-          <Link href={`http://coffee.localhost:3000`}>
-            <code className='block bg-gray-100 p-2 rounded'>
-              coffee.localhost:3000
-            </code>
-          </Link>
-          <br />
-          <Link href={`http://vibestore.localhost:3000`}>
-            {" "}
-            <code className='block bg-gray-100 p-2 rounded'>
-              vibestore.localhost:3000
-            </code>
-          </Link>
-          <br />
-          <Link href={`http://nexatech.localhost:3000`}>
-            {" "}
-            <code className='block bg-gray-100 p-2 rounded'>
-              nexatech.localhost:3000
-            </code>
-          </Link>
-          <br />
-          <Link href={`http://daraz-mart.localhost:3000`}>
-            {" "}
-            <code className='block bg-gray-100 p-2 rounded'>
-              daraz-mart.localhost:3000
-            </code>
-          </Link>
+
+        <div className='mt-4 space-y-2'>
+          {Object.entries(tenants).map(([slug, tenant]) => {
+            const url = `${protocol}://${slug}.${baseDomain}`;
+
+            return (
+              <Link key={slug} href={url}>
+                <code className='block bg-gray-100 p-2 rounded'>
+                  {slug}.{baseDomain} — {tenant.business_name}
+                </code>
+              </Link>
+            );
+          })}
         </div>
       </div>
     </div>
